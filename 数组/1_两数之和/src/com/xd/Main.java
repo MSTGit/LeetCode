f7bc9ba44665e2f5c962fad9be62f9f01405a24d
MRT <755737558@qq.com>
2020-01-28 11:32:12 +0800

    Create Main.java

---
+++ 数组/1_两数之和/src/com/xd/Main.java
@@ -0,0 +1,11 @@
+package com.xd;
+
+public class Main {
+    public static void main(String[] args) {
+        new Implement().twoSum(new int[]{0,1,1,0}, 0);
+    }
+    /*
+    * [-3,4,3,90]
+0
+    * */
+}
