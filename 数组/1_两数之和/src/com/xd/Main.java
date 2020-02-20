662466029ea65644f4ab23b3047719fb9695e6dc
MRT <755737558@qq.com>
2020-02-20 12:36:52 +0800

    Update Main.java

--- 数组/1_两数之和/src/com/xd/Main.java
+++ 数组/1_两数之和/src/com/xd/Main.java
@@ -2,7 +2,7 @@ package com.xd;
 
 public class Main {
     public static void main(String[] args) {
-        new Implement().twoSum(new int[]{0,1,1,0}, 0);
+        new Implement().twoSum(new int[]{2099,1,0,0}, 0);
     }
     /*
     * [-3,4,3,90]
