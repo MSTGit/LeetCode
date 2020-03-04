482b148ace2acb18739747930e602ae9f891fab7
MRT <755737558@qq.com>
2020-03-04 22:18:10 +0800

    Create Main.java

---
+++ 链表/24_两两交换链表中的节点/src/com/xd/Main.java
@@ -0,0 +1,12 @@
+package com.xd;
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        new Solution().swapPairs(node);
+    }
+}
